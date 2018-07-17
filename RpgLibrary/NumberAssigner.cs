6544c9241f5c18c6d900114e110c4fb1dbedfa22
Amirzhan Alimov <Amirzhan_Alimov@epam.com>
2018-07-17 17:53:08 +0600

    ZA WARUDO!!!

---
+++ RpgLibrary/NumberAssigner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgLibrary
+{
+    public static class NumberAssigner
+    {
+        static int nextNumber = 0;
+
+        public static int GetNextNumber()
+        {
+            nextNumber += 1;
+            return nextNumber;
+        }
+    }
+}
