27a6b8259003a4a188954b5ddcc6876014179c38
Amirzhan Alimov <Amirzhan_Alimov@epam.com>
2018-07-17 17:43:59 +0600

    Added inventory and loot

--- RpgLibrary/Player.cs
+++ RpgLibrary/Player.cs
@@ -12,12 +12,17 @@ namespace RpgLibrary
         public int ExperiencePoints { get; set; }
         public int Level { get; set; }
 
+        public List<InventoryItem> Inventory { get; set; }
+        public List<PlayerQuest> Quests { get; set; }
+
         public Player(int currentHitPoints, int maximumHitPoints, int gold, int experinecePoints, int level) 
             : base(currentHitPoints, maximumHitPoints)
         {
             Gold = gold;
             ExperiencePoints = experinecePoints;
             Level = level;
+            Inventory = new List<InventoryItem>();
+            Quests = new List<PlayerQuest>();
         }
     }
 }
